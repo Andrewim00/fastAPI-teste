d15616120b742722d63451ba637444c83a0e3d04
Andrewim00 <163583880+Andrewim00@users.noreply.github.com>
2025-05-07 00:00:27 +0000

    teste dicionário coisa

--- api/index.py
+++ api/index.py
@@ -8,23 +8,28 @@ app = FastAPI()
 template = Jinja2Templates(directory="templates")
 
 class SensorData(BaseModel):
-    id: int
-    ocupado: bool = True
+    vagas: dict[int, bool]
 
 
-vaga_status: str = "Livre"
+vaga_status: dict[int, str] = {
+    1: "Livre",
+    2: "Livre",
+    3: "Livre",
+    4: "Livre"
+}
 
 
 @app.put("/dados")
 async def receber_dados(dados: SensorData):
     global vaga_status
 
-    if dados.ocupado:
-        nova_vaga = "Ocupada"
-    else:
-        nova_vaga = "Livre"
+    for i in range(len(vaga_status)):
+        if dados.vagas[i+1]:
+            vaga_status[i + 1] = "Ocupado"
+        else:
+            vaga_status[i + 1] = "Livre"
 
-    vaga_status = nova_vaga
+
     return RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
 
 
