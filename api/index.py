9fa79f19907b7ad35e035978f3053537addcb32d
Andrewim00 <163583880+Andrewim00@users.noreply.github.com>
2025-05-07 00:27:52 +0000

    e

--- api/index.py
+++ api/index.py
@@ -8,10 +8,10 @@ app = FastAPI()
 template = Jinja2Templates(directory="templates")
 
 class SensorData(BaseModel):
-    vagas: dict
+    vagas: dict[str, bool]
 
 
-vaga_status: dict = {
+vaga_status: dict[int, str] = {
     1: "Livre",
     2: "Livre",
     3: "Livre",
@@ -24,7 +24,7 @@ async def receber_dados(dados: SensorData):
     global vaga_status
 
     for key in vaga_status:
-        if dados.vagas[key]:
+        if dados.vagas[str(key)]:
             vaga_status[key] = "Ocupado"
         else:
             vaga_status[key] = "Livre"
