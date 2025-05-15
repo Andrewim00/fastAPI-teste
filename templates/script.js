310cc136e9fc760e02bd8946eb010b81afd412e3
EAn022 <162371161+EAn022@users.noreply.github.com>
2025-05-15 11:47:54 +0000

    style:dashboard

--- templates/script.js
+++ templates/script.js
@@ -1,29 +1,29 @@
 async function atualizarVagas() {
-    const response = await fetch("/status");
-    const data = await response.json()
+  const response = await fetch("/status");
+  const data = await response.json()
+    
+  const listaCards = document.getElementById("cartoes");
+  listaCards.innerHTML = "";
 
-    const listaCards = document.getElementById("cartoes");
-    listaCards.innerHTML = "";
+  for (const [numero, status] of Object.entries(data.vaga)) {
+    const cardsDiv = document.createElement("div");
+    cardsDiv.id = "cards";
 
-    for (const [numero, status] of Object.entries(data.vaga)) {
-      const cardsDiv = document.createElement("div");
-      cardsDiv.id = "cards";
+    const card = document.createElement("div");
+    card.className = "card " + (status === "Livre" ? "livre" : "ocupado");
 
-      const card = document.createElement("div");
-      card.className = "card " + (status === "Livre" ? "livre" : "ocupado");
+    const titulo = document.createElement("h2");
+    titulo.textContent = `Vaga ${numero}`;
 
-      const titulo = document.createElement("h2");
-      titulo.textContent = `Vaga ${numero}`;
+    const paragrafo = document.createElement("p");
+    paragrafo.textContent = `Status: ${status}`;
 
-      const paragrafo = document.createElement("p");
-      paragrafo.textContent = `Status: ${status}`;
-
-      card.appendChild(titulo);
-      card.appendChild(paragrafo);
-      cardsDiv.appendChild(card);
-      listaCards.appendChild(cardsDiv);
-    }
+    card.appendChild(titulo);
+    card.appendChild(paragrafo);
+    cardsDiv.appendChild(card);
+    listaCards.appendChild(cardsDiv);
   }
+}
   
-  setInterval(atualizarVagas, 3000); // atualiza a cada 3 segundos
-  atualizarVagas(); // chama na primeira vez
+setInterval(atualizarVagas, 3000); // atualiza a cada 3 segundos
+atualizarVagas(); // chama na primeira vez
